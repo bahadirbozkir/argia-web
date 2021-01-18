cff8f6239893474d248830942b122d66e258f2c5
Barış <baris.barisci@hotmail.com>
2021-01-18 18:50:41 +0300

    server connection

--- pages/product/[pid].js
+++ pages/product/[pid].js
@@ -5,7 +5,36 @@ import PageBanner from '../../components/Common/PageBanner';
 import Footer from '../../components/Layouts/Footer';
 import ProductsDetailsTab from '../../components/SingleProducts/ProductsDetailsTab';
 
-const Post = () => {
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+  const posts = await res.json();
+  console.log(posts);
+
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post) => ({
+    params: { pid: post.id.toString() },
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${params.pid}`)
+  const post = await res.json()
+
+  // Pass post data to the page via props
+  return { props: { post } }
+}
+
+const Post = ({post}) => {
   const router = useRouter()
   const { pid } = router.query
 
@@ -31,14 +60,14 @@ const Post = () => {
 
             <div className="col-lg-6 col-md-12">
               <div className="product-details-desc">
-                <h3>72 LED 2835</h3>
+                <h3>{post.Name}</h3>
 
                 <div className="price">
-                  <span className="new-price">$14.00</span>
-                  <span className="old-price">$20.00</span>
+                  <span className="new-price">{post.Price} ₺</span>
+                  {/* <span className="old-price">$20.00</span> */}
                 </div>
 
-                <p>0,5W AC&DC / 10,7 - 24V / 15W</p>
+                <p>{post.ShortDescription}</p>
               </div>
             </div>
 
--- pages/shop.js
+++ pages/shop.js
@@ -3,96 +3,71 @@ import NavbarTwo from '../components/Layouts/NavbarTwo';
 import PageBanner from '../components/Common/PageBanner';
 import Footer from '../components/Layouts/Footer';
 
-class Shop extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <NavbarTwo />
-                <PageBanner 
-                    pageTitle="Ürünler" 
-                    homePageUrl="/" 
-                    homePageText="Ana Sayfa" 
-                    activePageText="Ürünler" 
-                    bgImgClass="item-bg2" 
-                />  
+export async function getServerSideProps(context) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+    console.log(res);
+    const datas = await res.json()
 
-                <div className="shop-area ptb-110">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-4 col-md-6 col-sm-6">
+    if (!datas) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: { datas }, // will be passed to the page component as props
+    }
+}
+
+const Shop = ({ datas }) => {
+    console.log(datas);
+    return (
+        <React.Fragment>
+            <NavbarTwo />
+            <PageBanner
+                pageTitle="Ürünler"
+                homePageUrl="/"
+                homePageText="Ana Sayfa"
+                activePageText="Ürünler"
+                bgImgClass="item-bg2"
+            />
+
+            <div className="shop-area ptb-110">
+                <div className="container">
+                    <div className="row">
+
+                        {datas.map((data) => (
+                            <div className="col-lg-4 col-md-6 col-sm-6" key={data.id}>
                                 <div className="single-product-box">
                                     <div className="product-image">
-                                        <a href="/product/1">
-                                            <img src="/images/shop/havuz-lambasi.jpg" alt="image" />
-                                            <img src="/images/shop/havuz-lambasi.jpg" alt="image" />
-                                        </a>   
-
-                                        <a href="/product/1" className="add-to-cart-btn">Ürün Detayı <i className="fas fa-cart-plus"></i></a>                                     
-                                    </div>
-
-                                    <div className="product-content">
-                                        <h3><a href="/product/1">72 LED 2835</a></h3>
-                                        <h5><a href="/product/1">0,5W AC&DC / 10,7 - 24V / 15W</a></h5>
-                                        <div className="price">
-                                            <span className="new">8.50₺</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-4 col-md-6 col-sm-6">
-                                <div className="single-product-box">
-                                    <div className="product-image">
-                                        <a href="/product/1">
+                                        <a href={'/product/' + data.id}>
                                             <img src="/images/shop/havuz-lambasi.jpg" alt="image" />
                                             <img src="/images/shop/havuz-lambasi.jpg" alt="image" />
                                         </a>
 
-                                        <a href="/product/1" className="add-to-cart-btn">Ürün Detayı <i className="fas fa-cart-plus"></i></a>
-
-                                        <div className="sale-btn">Sale!</div>
+                                        <a href={'/product/' + data.id} className="add-to-cart-btn">Ürün Detayı <i className="fas fa-cart-plus"></i></a>
                                     </div>
 
                                     <div className="product-content">
-                                        <h3><a href="/product/1">96 LED</a></h3>
-                                        <h5><a href="/product/1">2835-0,5W AC&DC / 10,7 - 17V / 25W</a></h5>
+                                        <h3><a href={'/product/' + data.Id}>{data.Name}</a></h3>
+                                        <h5><a href={'/product/' + data.Id}>{data.ShortDescription}</a></h5>
                                         <div className="price">
-                                            <span className="new">12.50₺</span>
-                                            <span className="old">20.50₺</span>
+                                            <span className="new">{data.Price} ₺</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-                            <div className="col-lg-4 col-md-6 col-sm-6">
-                                <div className="single-product-box">
-                                    <div className="product-image">
-                                        <a href="/product/1">
-                                            <img src="/images/shop/havuz-lambasi.jpg" alt="image" />
-                                            <img src="/images/shop/havuz-lambasi.jpg" alt="image" />
-                                        </a>
-
-                                        <a href="/product/1" className="add-to-cart-btn">Ürün Detayı <i className="fas fa-cart-plus"></i></a>
-                                    </div>
-
-                                    <div className="product-content">
-                                        <h3><a href="/product/1">45 LED</a></h3>
-                                        <h5><a href="/product/1">3030-1,5W AC&DC / 10,7 - 17V / 35W</a></h5>
-                                        <div className="price">
-                                            <span className="new">8.50₺</span>
-                                            <span className="old">12.50₺</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
+            </div>
 
-                <Footer />
-            </React.Fragment>
-        );
-    }
+            <Footer />
+        </React.Fragment>
+    );
 }
 
 export default Shop;
