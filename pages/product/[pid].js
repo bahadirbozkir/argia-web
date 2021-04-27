73d9e4523b9edfa03e8b8441bd642ebc8d78d27b
Bahadır BOZKIR <b.bahadirbozkir@gmail.com>
2021-04-27 21:44:24 +0300

    Revert "server connection"
    This reverts commit cff8f6239893474d248830942b122d66e258f2c5.

--- pages/product/[pid].js
+++ pages/product/[pid].js
@@ -5,36 +5,7 @@ import PageBanner from '../../components/Common/PageBanner';
 import Footer from '../../components/Layouts/Footer';
 import ProductsDetailsTab from '../../components/SingleProducts/ProductsDetailsTab';
 
-
-// This function gets called at build time
-export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
-  const posts = await res.json();
-  console.log(posts);
-
-  // Get the paths we want to pre-render based on posts
-  const paths = posts.map((post) => ({
-    params: { pid: post.id.toString() },
-  }))
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false }
-}
-
-// This also gets called at build time
-export async function getStaticProps({ params }) {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${params.pid}`)
-  const post = await res.json()
-
-  // Pass post data to the page via props
-  return { props: { post } }
-}
-
-const Post = ({post}) => {
+const Post = () => {
   const router = useRouter()
   const { pid } = router.query
 
@@ -60,14 +31,14 @@ const Post = ({post}) => {
 
             <div className="col-lg-6 col-md-12">
               <div className="product-details-desc">
-                <h3>{post.Name}</h3>
+                <h3>72 LED 2835</h3>
 
                 <div className="price">
-                  <span className="new-price">{post.Price} ₺</span>
-                  {/* <span className="old-price">$20.00</span> */}
+                  <span className="new-price">$14.00</span>
+                  <span className="old-price">$20.00</span>
                 </div>
 
-                <p>{post.ShortDescription}</p>
+                <p>0,5W AC&DC / 10,7 - 24V / 15W</p>
               </div>
             </div>
 
